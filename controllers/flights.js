ebaec9cd82f19426d95b94f2c93b296cb03cf883
EmmaZhang <emmazxd@gmail.com>
2024-02-24 17:02:10 +1100

    add sorting in index contorller

--- controllers/flights.js
+++ controllers/flights.js
@@ -2,31 +2,30 @@ const Flight = require("../models/flight");
 
 //functions
 async function index(req, res) {
-  const flights = await Flight.find({});
+  //sort in ascending order 1 indicates ascending order,-1 descending
+  const flights = await Flight.find().sort({ departs: 1 });
+
   res.render("flights/index", { flights });
 }
 
 async function newFlight(req, res) {
-  res.render("flights/new", { errMsg: "" });
+  //format flight departs date to yyyy-MM-mm
+  const newFlightDepart = new Flight();
+  const dt = newFlightDepart.departs;
+  let departsDate = `${dt.getFullYear()}-${(dt.getMonth() + 1)
+    .toString()
+    .padStart(2, "0")}`;
+  departsDate += `-${dt.getDate().toString().padStart(2, "0")}T${dt
+    .toTimeString()
+    .slice(0, 5)}`;
+
+  res.render("flights/new", { errMsg: "", departsDate });
 }
 
 async function create(req, res) {
   try {
     //airline-string formatting
     req.body.airline = req.body.airline.trim();
-
-    //format flight departs date to yyyy-MM-mm
-    const newFlightDepart = new Flight();
-    const dt = newFlightDepart.departs;
-    let departsDate = `${dt.getFullYear()}-${(dt.getMonth() + 1)
-      .toString()
-      .padStart(2, "0")}`;
-    departsDate += `-${dt.getDate().toString().padStart(2, "0")}T${dt
-      .toTimeString()
-      .slice(0, 5)}`;
-
-    //how to apply a format to mongoose ??????
-    req.body.departs = departsDate;
 
     //flight Num: 10-9999
     if (
@@ -41,7 +40,6 @@ async function create(req, res) {
 
     //link req.body to database
     await Flight.create(req.body);
-
     res.redirect("/flights/new");
   } catch (err) {
     res.render("flights/new", { errMsg: err.message });
