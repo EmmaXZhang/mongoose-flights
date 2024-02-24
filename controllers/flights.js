93e1349c0367a42f900098feda94b3274d4cb847
EmmaZhang <emmazxd@gmail.com>
2024-02-24 14:19:53 +1100

    add new/ create modue

--- controllers/flights.js
+++ controllers/flights.js
@@ -5,6 +5,38 @@ async function index(req, res) {
   res.render("flights/index", { flights });
 }
 
+async function newFlight(req, res) {
+  res.render("flights/new", { errMsg: "" });
+}
+
+async function create(req, res) {
+  //airline-string formatting
+  req.body.airline = req.body.airline.trim();
+
+  //flight Num: 10-9999
+  if (
+    isNaN(req.body.flightNo) ||
+    req.body.flightNo < 10 ||
+    req.body.flightNo > 9999
+  ) {
+    return res.render("flights/new", {
+      errMsg: "Flight number must be a number between 10 and 9999.",
+    });
+  }
+
+  //flight date:?????
+
+  try {
+    //link req.body to database
+    Flight.create(req.body);
+    res.redirect("/flights/new");
+  } catch (err) {
+    res.render("flights/new", { errMsg: err.message });
+  }
+}
+
 module.exports = {
   index,
+  new: newFlight,
+  create,
 };
--- routes/flights.js
+++ routes/flights.js
@@ -5,4 +5,9 @@ const flightsController = require("../controllers/flights");
 // display all flights
 router.get("/", flightsController.index);
 
+//enter new flight
+router.get("/new", flightsController.new);
+//create new flight
+router.post("/", flightsController.create);
+
 module.exports = router;
