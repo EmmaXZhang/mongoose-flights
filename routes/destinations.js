f065a0b213b86df18e77e4b5f5169143381f5203
EmmaZhang <emmazxd@gmail.com>
2024-02-26 17:35:14 +1100

    adding new destination

--- routes/destinations.js
+++ routes/destinations.js
@@ -2,6 +2,6 @@ var express = require("express");
 var router = express.Router();
 const destinationsController = require("../controllers/destinations");
 
-router.post("/:id/destinations", destinationsController.create);
+router.post("/flights/:id/destinations", destinationsController.create);
 
 module.exports = router;
